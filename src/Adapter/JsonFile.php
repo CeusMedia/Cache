82bb93dcec70eb40e9e492d30fb1d6049eb043bb
Christian Würker <kriss@ceusmedia.de>
2015-07-10 15:26:51 +0200

    Extend JSON adapter by general expiration and cleanup.

--- src/Adapter/JsonFile.php
+++ src/Adapter/JsonFile.php
@@ -17,6 +17,38 @@ class JsonFile extends \CeusMedia\Cache\AdapterAbstract implements \CeusMedia\Ca
 		$this->lock	= new \CeusMedia\Cache\Util\FileLock( $resource.'.lock' );
 	}
 
+	public function cleanup(){
+		$this->lock->lock();
+		try{
+			$contexts	= json_decode( $this->file->readString(), TRUE );
+			foreach( $contexts as $context => $entries ){
+				foreach( $entries as $key => $entry ){
+					if( $this->isExpiredEntry( $entry ) ){
+						unset( $contexts[$context][$key] );
+						$changed	= TRUE;
+					}
+				}
+			}
+			if( $changed )
+				file_put_contents( $this->resource, json_encode( $contexts ) );
+			$this->lock->unlock();
+		}
+		catch( Exception $e ){
+			$this->lock->unlock();
+			throw RuntimeException( 'Cleanup failed: '.$e->getMessage() );
+		}
+	}
+
+	protected function isExpiredEntry( $entry ){
+		if( $this->expiration ){
+			$now	= time();
+			$age	= (int) $entry['timestamp'] + $this->expiration;
+			if( $age <= $now || $entry['expires'] <= $now )
+				return TRUE;
+		}
+		return FALSE;
+	}
+
 	/**
 	 *	Removes all data pairs from storage.
 	 *	@access		public
@@ -45,13 +77,9 @@ class JsonFile extends \CeusMedia\Cache\AdapterAbstract implements \CeusMedia\Ca
 		if( !isset( $entries[$this->context][$key] ) )
 			return NULL;
 		$entry	= $entries[$this->context][$key];
-		if( $this->expiration ){
-			$now	= time();
-			$age	= (int) $entry['timestamp'] + $this->expiration;
-			if( $age <= $now || $entry['expires'] <= $now ){
-				$this->remove( $key );
-				return NULL;
-			}
+		if( $this->isExpiredEntry( $entry ) ){
+			$this->remove( $key );
+			return NULL;
 		}
 		return unserialize( $entry['value'] );
 	}
@@ -67,14 +95,9 @@ class JsonFile extends \CeusMedia\Cache\AdapterAbstract implements \CeusMedia\Ca
 		if( !isset( $entries[$this->context][$key] ) )
 			return FALSE;
 		$entry	= $entries[$this->context][$key];
-		if( $this->expiration ){
-			$now	= time();
-			$age	= (int) $entry['timestamp'] + $this->expiration;
-			if( $age <= $now || $entry['expires'] <= $now ){
-				$this->remove( $key );
-				return FALSE;
-			}
-
+		if( $this->isExpiredEntry( $entry ) ){
+			$this->remove( $key );
+			return FALSE;
 		}
 		return TRUE;
 	}
@@ -86,13 +109,12 @@ class JsonFile extends \CeusMedia\Cache\AdapterAbstract implements \CeusMedia\Ca
 	 */
 	public function index(){
 		$entries	= json_decode( $this->file->readString(), TRUE );
-		if( !isset( $entry[$this->context] ) )
+		if( !isset( $entries[$this->context] ) )
 			return array();
 		if( $this->expiration ){
 			$now	= time();
 			foreach( $entries[$this->context] as $key => $entry ){
-				$age	= (int) $entry['timestamp'] + $this->expiration;
-				if( $age <= $now || $entry['expires'] <= $now ){
+				if( $this->isExpiredEntry( $entry ) ){
 					$this->remove( $key );
 					unset( $entries[$this->context][$key] );
 				}
@@ -112,10 +134,20 @@ class JsonFile extends \CeusMedia\Cache\AdapterAbstract implements \CeusMedia\Ca
 		if( !isset( $entries[$this->context][$key] ) )
 			return FALSE;
 		$this->lock->lock();
-		unset( $entries[$this->context][$key] );
-		$this->file->writeString( json_encode( $entries ) );
-		$this->lock->unlock();
+		try{
+			unset( $entries[$this->context][$key] );
+			$this->file->writeString( json_encode( $entries ) );
+			$this->lock->unlock();
+		}
+		catch( Exception $e ){
+			$this->lock->unlock();
+			throw RuntimeException( 'Removing cache key failed: '.$e->getMessage() );
+		}
 		return TRUE;
+	}
+
+	public function removeByTags( $tags ){
+		
 	}
 
 	/**
@@ -128,16 +160,25 @@ class JsonFile extends \CeusMedia\Cache\AdapterAbstract implements \CeusMedia\Ca
 	 */
 	public function set( $key, $value, $expiration = NULL ){
 		$this->lock->lock();
-		$expiration	= $expiration ? $expiration : $this->expiration;
-		$entries	= json_decode( $this->file->readString(), TRUE );
-		$entries[$this->context][$key] = array(
-			'value'		=> serialize( $value ),
-			'timestamp'	=> time(),
-			'expires'	=> time() + (int) $expiration,
-//			'tags'		=> $tags,
-		);
-		$this->file->writeString( json_encode( $entries ) );
-		$this->lock->unlock();
+		try{
+			$expiration	= $expiration ? $expiration : $this->expiration;
+			$entries	= json_decode( $this->file->readString(), TRUE );
+			if( !isset( $entries[$this->context] ) )
+				$entries[$this->context]	= array();
+			$entries[$this->context][$key] = array(
+				'value'		=> serialize( $value ),
+				'timestamp'	=> time(),
+				'expires'	=> time() + (int) $expiration,
+	//			'tags'		=> $tags,
+			);
+			$this->file->writeString( json_encode( $entries ) );
+			$this->lock->unlock();
+			return TRUE;
+		}
+		catch( Exception $e ){
+			$this->lock->unlock();
+			throw RuntimeException( 'Setting cache key failed: '.$e->getMessage() );
+		}
 	}
 }
 ?>
